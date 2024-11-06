904bb6f497859db6cf18ab2a2482b5ce5a7e6bed
Bonolo Gaosenkwe <bonolo.kamolane@gmail.com>
2024-11-06 15:06:34 +0200

    DAOs and DTOs created

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,61 @@
+import { CreateUserDto } from '../dto/create.user.dto';
+import { PatchUserDto } from '../dto/patch.user.dto';
+import { PutUserDto } from '../dto/put.user.dto';
+
+import shortid from 'shortid';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:in-memory-dao');
+
+class UsersDao {
+  users: Array<CreateUserDto> = [];
+
+  constructor() {
+    log('Created new of UsersDao');
+  }
+
+  async addUser(user: CreateUserDto) {
+    user.id = shortid.generate();
+    this.users.push(user);
+    return user.id;
+  }
+
+  async getUsers() {
+    return this.users;
+  }
+
+  async getUsersById(userId: string) {
+    return this.users.find((user: { id: string }) => user.id === userId);
+  }
+
+  async putUserById(userId: string, user: PutUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string}) => obj.id === userId
+    );
+    this.users.splice(objIndex, 1, user);
+    return `${user.id} updated via put`;
+  }
+
+  async patchUserById(userId: string, user: PatchUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+    let currentUser = this.users[objIndex];
+    const allowedPatchFields = [
+      'password',
+      'firstName',
+      'lastName',
+      'permissionLevel',
+    ];
+    for (let field of allowedPatchFields) {
+      if (field in user) {
+        // @ts-ignore
+        currentUser[field] = user[field];
+      }
+    }
+    this.users.splice(objIndex, 1, currentUser);
+    return `${user.id} patched`;
+  }
+}
+
+export default new UsersDao();
